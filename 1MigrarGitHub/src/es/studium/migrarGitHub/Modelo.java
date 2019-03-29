64660cb7c3a62f58fd369cd2b32eb4706fff313d
DoctorSalt <joseamunozp@gmail.com>
2019-03-29 09:44:00 +0100

    Tercera Version

--- 1MigrarGitHub/src/es/studium/migrarGitHub/Modelo.java
+++ 1MigrarGitHub/src/es/studium/migrarGitHub/Modelo.java
@@ -1,9 +1,8 @@
 package es.studium.migrarGitHub;
 
 public class Modelo {
-	private void Escribir(String escrito){
+	public void Escribir(String escrito){
 		System.out.println(escrito);
 	}
-
 }
  

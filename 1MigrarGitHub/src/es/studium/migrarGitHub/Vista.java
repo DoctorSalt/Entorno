f025a8297beea3d09518d5d9cb62a0dfc9742c44
DoctorSalt <joseamunozp@gmail.com>
2019-03-29 10:04:34 +0100

    More fixs

--- 1MigrarGitHub/src/es/studium/migrarGitHub/Vista.java
+++ 1MigrarGitHub/src/es/studium/migrarGitHub/Vista.java
@@ -12,7 +12,7 @@ public class Vista extends Frame {
  
 	private static final long serialVersionUID = 1L;
 
-	TextArea texto = new TextArea();
+	TextArea texto = new TextArea(50,50);
 	Label etiqueta = new Label("Contar Palabras");
 	Button boton = new Button("Dale");
 	
@@ -21,14 +21,14 @@ public class Vista extends Frame {
 	
 	Vista(){
 	setTitle("Contar Palabras");
-	setSize(300,300);
 	setLocationRelativeTo(null);
 	setLayout(new GridLayout(2,1));
+	add(panel1);
+	add(panel2);
 	panel1.add(texto);
 	panel2.add(etiqueta);
 	panel2.add(boton);
-	add(panel1);
-	add(panel2);
+	setSize(500,500);
 	setResizable(false);
 	setVisible(true);
 	}
